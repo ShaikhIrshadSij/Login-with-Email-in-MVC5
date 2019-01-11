058371149a48af807064145756db9f07558f6f61
Dhiren Vaghani <dv@tcgitw.com>
2019-01-11 20:28:29 +0530

    add

---
+++ DemoEmailProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoEmailProject.Startup))]
+namespace DemoEmailProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
